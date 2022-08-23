337b0dab3fc9c4d52d28ee48817545f947840d64
Vlad-Los <apperkot@gmail.com>
2022-08-23 17:05:17 +0300

    23081111

---
+++ index.js
@@ -0,0 +1,19 @@
+// const age = 10;
+// const totalPrice = 200.74
+// const userName = "Chelsy" ;
+// const message = "Добро пожаловать" ;
+// const isOpen = true ;
+// const shouldConfirm = false;
+
+
+
+// const type = typeof true;
+
+// console.log (type);
+
+// console.log ('до');
+// alert( 'dlshvsldfh')
+// console.log ('после');
+
+const shouldRenew = confirm('Продлить подписку')
+console.log (shouldRenew);
