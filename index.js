187e3ae56f23556649171aa06900e927dc60ca34
Vlad-Los <apperkot@gmail.com>
2022-08-24 23:11:27 +0300

    2408

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+//задачи
+
+//конкатинация строк
+
+const firstName = 'Chelsy';
+const lastName = 'Emersld';
+const fullName = `${firstName}${lastName}`;
+
+console.log(fullName);
+
+
+
+
+
+
+//Длина строки свойстов Lenght
+
+// const message = 'В этой строке 111126 символов';
+// console.log(message.length);
+
+
+
+// 1.генерация рандомніх чисел
+
+// const max = 80;
+// const min = 10;
+
+// const result = Math.round(Math.random() * (max-min) + min);
+
+// console.log(result)
+
+
+
+
+// // 1.попросить ввести число
+// let base = prompt('Давай число');
+// base = Number (base);
+
+// console.log (base);
+
+// // 2.Попросить Ввести степень и сохранить в переменную
+
+// let power = prompt('Давай степень');
+// power = Number (power);
+// console.log(power);
+
+
+// // 3.Возвести введенные данные и вывести
+
+// const result = base ** power;
+// console.log(result);
+
+
+
+
+
+
+
+// let salary = 1300.1647;
+// salary = Number (salary.toFixed(2));
+// console.log(salary);
+
+
+
+
 // const age = 10;
 // const totalPrice = 200.74
 // const userName = "Chelsy" ;
@@ -15,5 +80,10 @@
 // alert( 'dlshvsldfh')
 // console.log ('после');
 
-const shouldRenew = confirm('Продлить подписку')
-console.log (shouldRenew);
+// const shouldRenew = confirm('Продлить подписку')
+// console.log (shouldRenew);
+
+// let quantity = prompt ( "Введите количество товаров");
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
