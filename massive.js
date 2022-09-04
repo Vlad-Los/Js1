c378cdd991b221877969bf1f9ce95e73330ff1ae
Vlad-Los <apperkot@gmail.com>
2022-09-04 18:24:00 +0300

    04091823

--- massive.js
+++ massive.js
@@ -40,10 +40,131 @@
 
 // --- Перебор (итерация ) массива
 
-const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
-console.table(friends);
-const lastIndex = friends.length -1
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.table(friends);
+// const lastIndex = friends.length - 1
 
-for (let i=0; i < friends.length; i += 1 ){
-console.log( friends[i]);
+// for (let i = 0; i <= lastIndex; i += 1 ){
+// console.log( friends[i]);
+// }
+// ------------
+// for (let i = 0; i <= lastIndex; i += 1) {
+//     friends[i] += '-1';
+
+//     console.table(friends);
+// }
+// ------------
+// for (const friend of friends) {
+//     console.log (friend);
+// }
+
+
+// --------------
+// Задача посчитать общую сумму покупок в корзине
+
+// const cart = [54, 28, 105, 70, 92, 17, 120];
+
+// // 2 сделать переменную до цикла
+
+// let total = 0;
+
+// // 1 перебрать массив
+
+// for (let i = 0; i < cart.length; i += 1){
+// console.log(cart[i])
+
+// // 3 каждый элемент приплюсовать к тоталу
+
+// // total = total + cart [i];
+// total += cart [i];
+
+// }
+
+// console.log('total:', total);
+
+
+// -------------Вариант 2 через for
+
+// const cart = [54, 28, 105, 70, 92, 17, 120];
+
+// // 2 сделать переменную до цикла
+
+// let total = 0;
+
+// for (const value of cart) {
+//     total =+ value;
+// }
+
+// console.log('total:', total);
+
+
+// -----------Вариант 3 добавить 20 % налога
+
+// const cart = [54, 28, 105, 70, 92, 17, 120];
+
+// // 2 сделать переменную до цикла
+
+// let total = 0;
+
+
+
+// for (let i =0; i < cart.length; i += 1) {
+//     cart[i] = Math.round(cart[i] * 1.1);
+// }
+
+// console.log(cart);
+// Если елемент массива не изменяеться то For of если менятеься то с счетчиком
+
+
+// // Задача напиши скрипт подсчитывающий сумму всех чисел в масииве
+
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+
+// // 1 переменная тотал
+// let total = 0;
+
+
+
+// // 2 перебрать массив
+
+// for(let i = 0; i < numbers.length; i += 1) {
+// const number = numbers[i];
+// console.log(number);
+
+
+
+// // 3 на каждой иттерации проверить елемент на четность
+
+// if (number % 2 === 0) {
+//     console.log('Четное!!!!');
+
+// // 4 если четный плюсуем к тотал
+
+// total += number;
+
+// }
+
+// }
+// console.log('total:',total);
+// -------------------------------------
+
+// ВАриант через for of
+
+
+const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+
+// 1 переменная тотал
+let total = 0;
+
+
+for (const number of numbers){
+    console.log(number);
+
+if (number % 2 === 0){
+    console.log('Четное!!!!');
+
+    total += number;
 }
+}
+
+console.log('total:', total);
