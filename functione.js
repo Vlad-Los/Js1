d87e4c44380719d122e3cde4793f73d71f2f0384
Vlad-Los <apperkot@gmail.com>
2022-09-09 15:40:59 +0300

    09091574

--- functione.js
+++ functione.js
@@ -53,24 +53,122 @@
 
 // console.log('Total: ',total);
 
-const calculateTotalPrice = function (items){
-    console.log('items внутри функции:', items);
+// const calculateTotalPrice = function (items){
+//     console.log('items внутри функции:', items);
 
-    let total = 0;
+//     let total = 0;
 
-    for (const item of items) {
-        total += item;
-    };
+//     for (const item of items) {
+//         total += item;
+//     };
 
-    return total;
+//     return total;
 
+// };
+
+// const r1 = calculateTotalPrice([1, 2, 3]);
+
+// console.log(`Общая сумма покупок ${r1}`);
+
+// console.log(calculateTotalPrice([1, 2, 3]));
+// console.log(calculateTotalPrice([5, 10, 15, 20]));
+// console.log(calculateTotalPrice([100, 200, 300]));
+
+// --------------------------------------
+// Напиши функцию  logItems(items) для перебора и логирования массива
+
+// const logItems = function (items) {
+//     for (const item of items){
+//         console.log(item);
+//     }
+// };
+
+// logItems(['dffddfd','sdfkgdlfkfdl','lsdkfldf', 'kldfhdl']);
+// logItems([1, 2, 3, 4, 5]);
+// logItems(['dffdd','sdfkglfkfdl','lkfldf', 'kfhdl']);
+
+// ---------------------------------------------
+// Напиши функцию findLogin(allLogins, login) для поиска логина
+// - Если логина нет, вывести сообщение 'Пользователь [логин] не найден'
+// - Если нашли логин, вывести сообщение 'Пользователь [логин] найден.'
+
+// const logins = ['sssds', 'kfdfh', 'poly1scute' , 'jhg'];
+
+
+// const findLogin = function (allLogins, loginToFind) {
+// for(const login of allLogins){
+//     if(login === loginToFind) {
+        
+//         return 'Пользователь ${loginToFind} найден.';
+
+//     }
+// }
+// return 'Пользователь ${loginToFind} не найден.';
+// };
+// console.log(findLogin(logins, 'ssddfsds'));
+// console.log(findLogin(logins, 'kfjjkhgdfh'));
+// console.log(findLogin(logins, 'poly1scute'));
+// console.log(findLogin(logins, 'jhgjh'));
+
+// ------------------------------------------
+// решение через тернавник
+
+
+
+// const logins = ['sssds', 'kfdfh', 'poly1scute' , 'jhg'];
+
+// const findLogin = function (allLogins, loginToFind) {
+
+// return allLogins.includes(loginToFind)
+// ?'Пользователь ${loginToFind} найден.'
+// :'Пользователь ${loginToFind} не найден.';
+// };
+
+// console.log(findLogin(logins, 'ssddfsds'));
+// console.log(findLogin(logins, 'kfjjkhgdfh'));
+// console.log(findLogin(logins, 'poly1scute'));
+// console.log(findLogin(logins, 'jhgjh'));
+
+// ---------------------------------------------
+
+// напиши функцию findSmalesNumber(numbers) для поиска самого малого числа в масииве
+// при условии что числа уникальные не повоторяються
+
+// const findSmallesNumber = function (numbers) {
+//     let smallestNumber = numbers[0];
+//     for (const number of numbers) {
+//         if (number < smallestNumber) {
+//             smallestNumber = number;
+//         }
+//         }
+// return smallestNumber;
+// };
+
+// console.log(findSmallesNumber([3, 5, 23,25,22]));
+// console.log(findSmallesNumber([5, 15, 213,215,122]));
+// console.log(findSmallesNumber([13, 15, 213,215,221]));
+
+// --------------------------------------------------
+
+// Напиши функцию ChangeCase(string) которая заменяет регистр
+// каждого символа в строке на противоположный 
+// Напримерб если строка JavaScript, то на выходе должноа быть 
+// строка JAVAsCRIPT
+
+
+
+const changeCase = function(string) {
+    const letters = string.split('');
+    let invertedString = '';
+
+    for(const letter of letters){
+        const isInLowerCase = letter === letter.toLowerCase();
+        invertedString += isEqual ? letter.toUpperCase(); letter.toLowerCase();
+    }
+
+    return invertedString;
 };
 
-const r1 = calculateTotalPrice([1, 2, 3]);
-
-console.log(`Общая сумма покупок ${r1}`);
-
-console.log(calculateTotalPrice([1, 2, 3]));
-console.log(calculateTotalPrice([5, 10, 15, 20]));
-console.log(calculateTotalPrice([100, 200, 300]));
-
+console.log (changeCase('dfgdfGGG'));
+console.log (changeCase('DKJFHDkkkkhs'));
+console.log (changeCase('dKDJFSADKllllddd'));
